cc6d335d23593a7357677893cc0fae5eaaadd507
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2020-05-01 17:43:13 +0300

    marked spring.jpa.hibernate.ddl-auto = create for tests

--- backend/src/main/resources/sql-scripts/data.sql
+++
@@ -1,6 +0,0 @@
--- USER
--- non-encrypted passwords:
--- regular.user@test.com:regular
--- manager.user@test.com:manager
-INSERT INTO app_user (id, username, password, role) VALUES (1, 'regular.user@test.com', '$2a$10$TKloylsuFftdsXM7E8BAB.o556UTDTYKWwmgSaPyS4QE1zY8BI/tC', 'REGULAR_USER');
-INSERT INTO app_user (id, username, password, role) VALUES (2, 'manager.user@test.com', '$2a$10$65v6zXWZll9zIUgXhp4kQ.nfmrKi5NeWXEfEEKi.0woWvYPRtEeQi', 'MANAGER_USER');
--- backend/src/main/resources/sql-scripts/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE app_user (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  role varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-);
